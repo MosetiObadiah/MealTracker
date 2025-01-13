9165c27230429aa673747004397423e8c0507658
moseti <mosetioba01@gmail.com>
2025-01-13 07:51:36 +0300

    minor upgrades on tools and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,14 +30,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.credentials:credentials:1.3.0-alpha01")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.credentials:credentials:1.3.0")
     // optional - needed for credentials support from play services, for devices running
     // Android 13 and below.
-    implementation("androidx.credentials:credentials-play-services-auth:1.3.0-alpha01")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
